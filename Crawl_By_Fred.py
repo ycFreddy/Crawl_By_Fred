3e64603c07015936162c13a0085da36a85aa7c1d
ycFreddy <61626752+ycFreddy@users.noreply.github.com>
2023-11-29 11:03:32 +0100

    Update Crawl_By_Fred.py

--- Crawl_By_Fred.py
+++ Crawl_By_Fred.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from collections import deque
 from html.parser import HTMLParser
 from urllib.parse import urlparse
+import pandas as pd
 import csv
 
 HTTP_URL_PATTERN = r'^http[s]{0,1}://.+$'
@@ -43,23 +44,19 @@ def clean_liens(liens):
             if re.findall(r"facebook\b", str(lien)) or re.findall(r"twitter\b", str(lien)) or re.findall(r"instagram\b", str(lien)) or re.findall(r"pinterest\b", str(lien)):
                 continue            
             clean_link = "https://" + local_domain + "/" + lien
-
         if clean_link is not None:
             if clean_link.endswith("/"):
                 clean_link = clean_link[:-1]
             clean_links.append(clean_link)            
     return list(set(clean_links))
 
-def remplirCSV (nomfichier, ligne):
-    titre = ""
-    meta_description = ""
-    h1 = ""
+def remplirCSV (nomfichier, ligne):    
+    titre = meta_description = h1 = "" 
     fieldnames = ["Status code", "Url", "Titre", "Meta Description", "H1"]
     if not os.path.exists(nomfichier):
         with open(nomfichier, 'w', encoding="UTF-8", newline='') as csvfile:            
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-    
+            writer.writeheader()    
     with open(nomfichier, 'a', encoding="UTF-8", newline='') as csvfile:            
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)        
         if ligne.status == 200:    
@@ -71,16 +68,18 @@ def remplirCSV (nomfichier, ligne):
 def crawl (url, nomfichier):
     lienvu = set([url])
     infos = PageInfos(url)
-    queue = infos.liens_clean
+    queue = infos.liens_clean    
     while queue:
         lien = queue.pop()         
         if lien not in lienvu:
             lienInfos = PageInfos(lien)
             print("Status:", lienInfos.status, "Url:", lien)
-            remplirCSV (nomfichier, lienInfos)
+            remplirCSV (nomfichier + ".csv", lienInfos)
             if lienInfos.status == 200:
                 queue.extend(lienInfos.liens_clean)
             lienvu.add(lien)
-        queue = list(set(queue))
+        queue = list(set(queue))    
+    df=pd.read_csv(nomfichier + ".csv", delimiter=',')
+    df.to_html(nomfichier + ".html")
         
-crawl(site_url,"export_crawl.csv")
+crawl(site_url,"export_crawl")
